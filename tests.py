144c3b48e90194ac8415dc17c453bfc83b8647e3
sebastian.nachtigall <sebastian.nachtigall@googlemail.com>
2023-02-13 11:12:19 +0100

    confused with indentation

--- tests.py
+++ tests.py
@@ -2,5 +2,6 @@ import unittest
 from download_new_odt_attachments import *
 
 class MyFirstTests(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello_world(), 'hello world')
+
+def test_hello(self):
+    self.assertEqual(hello_world(), 'hello world')
