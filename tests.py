9568e24078f3eb87cc345547f4f3f4596b32341c
sebastian.nachtigall <sebastian.nachtigall@googlemail.com>
2023-02-13 11:20:42 +0100

    Alrighr lets try again

--- tests.py
+++ tests.py
@@ -1,7 +1,14 @@
 import unittest
-from download_new_odt_attachments import *
-
-class MyFirstTests(unittest.TestCase):
-
-    def test_hello(self):
-        self.assertEqual(hello_world(), 'hello world')
+class StringMethodstest(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('python'.upper(), 'PYTHON')
+    def test_isupper(self):
+        self.assertTrue('PYTHON'.isupper())
+        self.assertFalse('python'.isupper())
+    def test_split(self):
+        s = 'hello python'
+        self.assertEqual(s.split(), ['hello', 'python'])
+        with self.assertRaises(TypeError):
+            s.split(2)
+if __name__ == '__main__':
+    unittest.main()
